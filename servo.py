ef3b68a352350645a8c41892fd135784c8d11808
Sofiane Bebert <bebertsofiane@gmail.com>
2021-02-18 13:18:28 +0000

    ✨ add continuous servo support

--- servo.py
+++ servo.py
@@ -15,7 +15,9 @@ class Servo:
         = Servo(pin0).write_angle(90)
     """
 
-    def __init__(self, pin, freq=50, min_us=600, max_us=2400, angle=180):
+    def __init__(
+        self, pin, freq=50, min_us=600, max_us=2400, angle=180
+    ):
         self.min_us = min_us
         self.max_us = max_us
         self.us = 0
@@ -23,14 +25,14 @@ class Servo:
         self.angle = angle
         self.analog_period = 0
         self.pin = pin
-        analog_period = round((1/self.freq) * 1000)  # hertz to miliseconds
+        analog_period = round((1 / self.freq) * 1000)  # hertz to miliseconds
         self.pin.set_analog_period(analog_period)
 
     def write_us(self, us):
         us = min(self.max_us, max(self.min_us, us))
         duty = round(us * 1024 * self.freq // 1000000)
+        print(duty)
         self.pin.write_analog(duty)
-        self.pin.write_digital(0)  # turn the pin off
 
     def write_angle(self, degrees=None):
         degrees = degrees % 360
